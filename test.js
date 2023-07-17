24109c56e0e046380e314b7de1886caee0834b03
Guilherme-07062002 <guilherme762002@gmail.com>
2023-07-17 16:32:37 -0300

    load and stress testing

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 import http from "k6/http";
 import { sleep } from "k6";
 
+export let options = {
+  stages: [
+    { duration: "10s", target: 10 }, // 10 usuários por 10 segundos
+    { duration: "20s", target: 50 }, // 50 usuários por 20 segundos
+    { duration: "20s", target: 100 }, // 50 usuários por 20 segundos
+    { duration: "10s", target: 0 }, // Sem usuários por 10 segundos
+  ],
+};
+
 export default function () {
-  const url = "https://api.example.com"; // Replace with your API URL.
+  const url = "https://api.example.com"; // Substitua pela URL da sua API.
 
   const payload = JSON.stringify({
     data: "example",
@@ -14,10 +23,9 @@ export default function () {
     },
   };
 
-  let response = http.post(url, payload, params);
+  let response = http.get(url, payload, params);
 
-  console.log(`Status: ${response.status}`);
-  console.log(`Body: ${response.body}`);
+  // Realize asserções adicionais na resposta, se necessário
 
-  sleep(1); // Wait 1 second before sending the next request
+  sleep(1); // Aguarde 1 segundo antes de enviar a próxima requisição
 }
